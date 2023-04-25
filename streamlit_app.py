d3d281f490b0257dffab297cd3ff560db1089d28
Chinar Sakrikar <118957493+Chinarsak@users.noreply.github.com>
2023-04-25 15:29:17 +0530

    Update streamlit_app.py
    updated some text

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit as st
 
 # Add a title
 st.title("My Streamlit App")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')

d5f46cffebaaaaba7464ae2a602fb9e5f66ce366
Chinar Sakrikar <118957493+Chinarsak@users.noreply.github.com>
2023-04-25 16:48:22 +0530

    Update streamlit_app.py
    added loc at last line

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.text('Kale, Spinach & Rocket Smoothie')
 st.text('Hard-Boiled Free-Range Egg')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt",header=None)
 st.dataframe(my_fruit_list)
 
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -17,3 +17,4 @@ st.multiselect("Pick some fruits:", list(my_fruit_list.index))
 my_fruit_list = my_fruit_list.set_index(1)
 
 st.multiselect("Pick some fruits", my_fruit_list.index.tolist(), ['Peach', 'Pineapple'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]

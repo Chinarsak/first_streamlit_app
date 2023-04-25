e03680c16dd4c134dfa8ce6ddf282975d65acc1b
Chinar Sakrikar <118957493+Chinarsak@users.noreply.github.com>
2023-04-25 15:30:31 +0530

    Update streamlit_app.py
    updated some typos

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
-
 import streamlit as st
 
-# Add a title
-st.title("My Streamlit App")
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+st.header('Breakfast Menu')
+st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')

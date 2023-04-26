37a6ec0954e76ad3027e5412365f05da07450332
Chinar Sakrikar <118957493+Chinarsak@users.noreply.github.com>
2023-04-26 16:10:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ st.dataframe(my_data_row)
 fruit_choice_2 = st.text_input('What is your second fruit choice?', 'Apple')
 st.write('The user entered', fruit_choice_2)
 # Construct the SQL INSERT statement
-insert_stmt = "INSERT INTO PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values('from streamlit'))
+insert_stmt = "INSERT INTO PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values('from streamlit'))"
 # Execute the INSERT statement
 my_cur.execute(insert_stmt)
 # my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])

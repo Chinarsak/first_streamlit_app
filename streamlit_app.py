a9c4ecdb4958d6def248e764a4e274cf32b2f14b
Chinar Sakrikar <118957493+Chinarsak@users.noreply.github.com>
2023-04-26 13:05:44 +0530

    Update streamlit_app.py
    added snowflake and python connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-
+import snowflake.connector
 st.header('Breakfast Menu')
 st.text('Omega 3 & Blueberry Oatmeal')
 st.text('Kale, Spinach & Rocket Smoothie')

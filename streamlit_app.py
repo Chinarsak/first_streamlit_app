2ea54eed613ada69d36c7e35fb2ea731d7835fd3
Chinar Sakrikar <118957493+Chinarsak@users.noreply.github.com>
2023-04-26 11:35:42 +0530

    Update streamlit_app.py
    normalized json

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,11 +10,16 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt",header=None)
 st.dataframe(my_fruit_list)
 
-
+st.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response.json())
-st.header("Fruityvice Fruit Advice!")
+
+
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+st.dataframe(fruityvice_normalized)
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 # st.multiselect("Pick some fruits:", list(my_fruit_list.index))
